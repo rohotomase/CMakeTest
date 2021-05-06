936e6efff8682a471426cec1640a80afd013f2fc
Scott Mooney <scott.mooney@citrix.com>
2021-05-06 12:07:52 -0400

    Add project files.

---
+++ CMakeTest.cpp
@@ -0,0 +1,17 @@
+﻿// CMakeTest.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeTest.h"
+#include <gtest/gtest.h>
+
+using namespace std;
+
+TEST(CMakeTestTests, BasicTest) {
+	EXPECT_TRUE(true);
+}
+
+int main()
+{
+	testing::InitGoogleTest();
+	return RUN_ALL_TESTS();
+}
